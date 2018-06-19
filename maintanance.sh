3784875cf0e0823894c7c3e8a5e71662ede86b5c
naoki <n.globe.us@gmail.com>
2018-06-19 20:01:36 +0900

    fix some

--- maintanance.sh
+++ maintanance.sh
@@ -2,35 +2,43 @@
 
 set -ex
 
+# please set maximam number of buckup data.
+MAX_NUM_BACKUP=1
+
 wp cli update --yes
 
 BASE_DIR=~/apps/wordpress/htdocs
-
 cd ${BASE_DIR}
 
+# create wp-backup directory.
 if [ ! -e wp-backup/ ]; then
   mkdir wp-backup
   mv maintanance.sh wp-backup/
 fi
 
+# copy files to backup.
 cd ${BASE_DIR}/wp-content
 cp -r plugins/ themes/ uploads/ -t ${BASE_DIR}/wp-backup
+
+# export database.
 wp db export ${BASE_DIR}/wp-backup/wordpress.sql
+
 cd ${BASE_DIR}/wp-backup
+
+# zip backup files.
 zip -r $(date +%Y%m%d%H%M%S) plugins/ themes/ uploads/ wordpress.sql
 rm -r plugins/ themes/ uploads/ wordpress.sql
+
 
 FILE_NUM=$(find ./ -name "*.zip" | wc -l)
 ZIP=($(find ./ -name "*.zip" -type f | sort -n))
 
+# if the number of backup data is more than limit, delete oldest one.
 if [ $FILE_NUM -gt 1  ]; then
-
-rm ${ZIP[0]}
-unset ZIP[0]
-ZIP=("${ZIP[@]}")
-
+  rm ${ZIP[0]}
+  unset ${ZIP[0]}
+  ZIP=("${ZIP[@]}")
 fi
-
 
 wp core update
 wp plugin update --all
